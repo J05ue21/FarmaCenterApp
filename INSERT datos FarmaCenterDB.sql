9848bc46a1b8e8d7cef9ca0ff1e4991ecadd0217
J05ue21 <90211500+J05ue21@users.noreply.github.com>
2025-09-24 17:10:40 -0600

    Añadir script de base de datos FarmaCenteDB
    Se agregó script sql de creación de base de datos e inserción de datos a FarmaCenterDb

---
+++ INSERT datos FarmaCenterDB.sql
@@ -0,0 +1,16 @@
+﻿USE FarmaCenterDb;
+GO
+
+INSERT INTO Productos (Nombre, Precio, Descripcion, Stock)
+VALUES
+  
+  ('Loratadina 10mg',        4.50, 'Antihistamínico - 10 tabletas',                           180),
+  ('Acetilcisteína 200mg',   3.75, 'Mucolítico jarabe 150ml',                                 90),
+  ('Clorhexidina 0.12%',     7.00, 'Enjuague bucal antiséptico 300ml',                        70),
+  ('Gel antibacterial 70%',  2.50, 'Gel desinfectante de manos - 250ml',                      250),
+  ('Protector Solar SPF50', 12.00, 'Loción solar resistente al agua 200ml',                  100),
+  ('Sales de Rehidratación', 3.20, 'Polvo para solución oral - 10 sobres',                    150),
+  ('Vitamina C 1000mg',      6.80, 'Tabletas liberación prolongada - 30 unidades',            85),
+  ('Ibuprofeno 600mg',       5.50, 'Analgesico antiinflamatorio - 20 cápsulas',               110),
+  ('Aspirina 500mg',         2.80, 'Analgésico y antipirético - 20 tabletas',                  140);
+GO
---
+++ farmaCenter.sql
@@ -0,0 +1,48 @@
+﻿USE master;
+GO
+
+IF DB_ID(N'FarmaCenterDb') IS NULL
+    CREATE DATABASE FarmaCenterDb;
+GO
+
+USE FarmaCenterDb;
+GO
+
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250924175458_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Productos] (
+        [Id] int NOT NULL IDENTITY,
+        [Nombre] nvarchar(max) NOT NULL,
+        [Precio] decimal(18,2) NOT NULL,
+        [Descripcion] nvarchar(max) NOT NULL,
+        [Stock] int NOT NULL,
+        CONSTRAINT [PK_Productos] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250924175458_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250924175458_InitialCreate', N'9.0.9');
+END;
+
+COMMIT;
+GO
+
